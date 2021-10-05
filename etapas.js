714c3d4fd862dc671394e449a3cdb9dd52a5907c
Alan de Santana Thimotheo <alanthimotheo@hotmail.com>
2021-10-05 00:02:56 -0300

    Urna eletrônica versão desktop, desenvolvida utilizando HTML, CSS e JavaScript puros.

---
+++ etapas.js
@@ -0,0 +1,94 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR ',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Phil Foden',
+                partido: 'Não deu',
+                fotos: [
+                    {url:'foden.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Kai Havertz',
+                partido: 'Atual Campeão',
+                fotos: [
+                    {url: 'havertz.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '11111',
+                nome: 'Kyllian Mbappe',
+                partido: 'Nunca serão',
+                fotos: [
+                    {url: '11.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '10101',
+                nome: 'Paulo Dybala',
+                partido: 'Velha Bota',
+                fotos: [
+                    {url: 'dybala.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '11010',
+                nome: 'Ansu Fati',
+                partido: 'Visca Barça',
+                fotos: [
+                    {url: 'fati.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '09',
+                nome: 'Memphis Depay',
+                partido: 'Visca Barça',
+                vice: 'Pique',
+                fotos: [
+                    {url: 'depay.jpg', legenda:  'Prefeito'},
+                    {url: 'pique.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '07',
+                nome: 'CR7',
+                partido: 'Red Devils',
+                vice: 'Pogba',
+                fotos: [
+                    {url: '07.jpg', legenda: 'Prefeito'},
+                    {url: 'pogba.jfif', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Lionel Messi',
+                partido: 'Nunca Serão',
+                vice: 'Neymar',
+                fotos: [
+                    {url: '10.jfif', legenda: 'Prefeito'},
+                    {url: 'neymar.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'Romelu Lukaku',
+                partido: 'Atual Campeão',
+                vice: 'Jorginho',
+                fotos: [
+                    {url: 'lukaku.jpg', legenda: 'Prefeito'},
+                    {url: 'jorginho.jfif', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
